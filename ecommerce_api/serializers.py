4a5a5cda4131daa31d4a056bd3f0d4191f2af196
shivasaicharankasarla <shivasaicharankasarla03@gmail.com>
2023-10-03 09:17:53 +0530

    created login and registration

---
+++ ecommerce_api/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from . import models
+from django.contrib.auth.models import User
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Product
+        fields = ('name','description','price')
+        extra_kwargs = {
+            'name': {'required': True,},
+            'description': {'required': True},
+            'price': {'required': True}
+        }
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+        )
+        # Create a Customer instance and associate it with the user
+        customer = Customer(user=user)
+        customer.save()
+        return user
+
+
+class CustomerProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Customer
+        fields = '__all__'  # Include any additional fields
+
+# In a view or serializer
+#customer = models.Customer.objects.get(user=request.user)
+#customer_profile_data = CustomerProfileSerializer(customer).data
