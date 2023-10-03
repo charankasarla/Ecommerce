ca80ef05c663afe27a578f13651a361d4e4d4412
shivasaicharankasarla <shivasaicharankasarla03@gmail.com>
2023-10-03 09:15:55 +0530

    created login and registration

--- ecommerce_api/views.py
+++ ecommerce_api/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+class ProductListCreateView(APIView):
+    #serializer_class = serializers.ProductSerializer
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        #print(serializer.data)
+        return Response(serializer.data)
+
+    def post(self, request,format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            product = Product(
+                name=serializer.validated_data['name'],
+                description=serializer.validated_data['description'],
+                price=serializer.validated_data['price'],
+                #quantity=serializer.validated_data['quantity'],
+                # Add any other fields as needed
+            )
+            product.save()
+            #print(serializer.data)
+            response_serializer = ProductSerializer(product)
+            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductRetrieveUpdateDestroyView(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return Response({'message': 'User logged in successfully'}, status=status.HTTP_200_OK)
+        return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
